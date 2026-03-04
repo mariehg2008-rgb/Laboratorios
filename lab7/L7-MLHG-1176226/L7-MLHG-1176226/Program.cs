189b60f044df24b92cfc3c36b8652a72ec77a905
mariehg2008-rgb <mariehg2008@gmail.com>
2026-03-04 09:44:18 -0600

    Laboratorio 7

---
+++ lab7/L7-MLHG-1176226/L7-MLHG-1176226/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.SymbolStore;
+class Program
+{
+    static void Main()
+    {
+        //EJERCICIO #1
+        Console.WriteLine("EJERCICIO #1");
+        Console.WriteLine("Ingrese la cantidad de números que desea sumar: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int conta = 0;
+        int suma = 0;
+        while (num < 0)
+        {
+            Console.WriteLine("El número tiene que ser mayor a 0, vuelve a ingresar el número");
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+        while (conta <= num)
+        {
+            suma = suma + conta;
+            conta++;//El contador va abajo de la operación porque sino comienza con el valor 1 en vez de 0 y da una suma incorrecta.
+        }
+        Console.WriteLine("La suma de sus números es " + suma);
+        
+    }
+}
